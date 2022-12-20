866b9b85572e49405ba597aabf41c510109a9861
believelody <believelody@gmail.com>
2022-12-20 19:57:09 +0100

    project setup and define layout

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("express");
+const bodyParser = require("body-parser");
+const port = 3000;
+const app = express();
+const path = require("path");
+const EJSLayout = require('express-ejs-layouts');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// => Here we expose the views so it can be rendered.
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(EJSLayout);
+// => Here we expose your dist folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/", (req, res) => {
+    res.render("index", { layout: !req.headers['hx-request'] });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", { layout: !req.headers['hx-request'] });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", { layout: !req.headers['hx-request'] });
+});
+
+app.get("/teams", (req, res) => {
+    res.render("teams", { layout: !req.headers['hx-request'] });
+});
+
+app.get("/blog", (req, res) => {
+    res.render("blog", { layout: !req.headers['hx-request'] });
+});
+
+app.listen(port, (req, res) => {
+    console.log("Server running");
+});
