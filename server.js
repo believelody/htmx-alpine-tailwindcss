a929c25a59d6c0f07faf00b2dfdf4c29d6b53652
believelody <believelody@gmail.com>
2022-12-24 21:23:21 +0100

    refactoriised routes and defined htmx middleware

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const { array, misc, string } = require('useful-handlebars-helpers');
 const bodyParser = require("body-parser");
 const port = 8000;
 const path = require("path");
-const { routes } = require('./routes')
+const { routes } = require('./routes');
+const homeRoute = require('./routes/home');
+const aboutRoute = require('./routes/about');
+const contactRoute = require('./routes/contact');
+const teamsRoute = require('./routes/teams');
+const blogRoute = require('./routes/blog');
+const loginRoute = require('./routes/login');
 
 const app = express();
 
@@ -16,13 +22,10 @@ hbs.registerHelper('let', function (ctx) {
         ctx.data.root[key] = value;
     })
 });
-
 hbs.registerHelper('routes', ctx => {
     return routes;
 });
-
 [array, misc, string].forEach(helper => hbs.registerHelper(helper));
-
 hbs.registerHelper('partial', function (partialName, options) {
     if (!partialName) {
         console.error('No partial name given.');
@@ -35,7 +38,6 @@ hbs.registerHelper('partial', function (partialName, options) {
     }
     return new hbs.SafeString(partial(options.hash));
 });
-
 hbs.registerHelper('toString', function (name, options) {
     if (!name) {
         console.error('No name given.');
@@ -43,26 +45,22 @@ hbs.registerHelper('toString', function (name, options) {
     }
     return name;
 });
-
 hbs.registerHelper('switch', function(value, options) {
     this.switchValue = { value, found : false };
     return options.fn(this);
 });
-
 hbs.registerHelper('case', function(value, options) {
     if (value == this.switchValue.value) {
         this.switchValue.found = true;
         return options.fn(this);
     }
 });
-
 hbs.registerHelper('default', function(options) {
     return !this.switchValue.found ? options.fn(this) : null; ///We can add condition if needs
 });
-
 hbs.registerHelper('withDefault', (testedValue, defaultValue, options) => {
     return testedValue || defaultValue;
-})
+});
 
 // => Here we expose the views so it can be rendered.
 app.engine('.hbs', hbs.express4({
@@ -72,55 +70,21 @@ app.engine('.hbs', hbs.express4({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'views'));
 // => Here we expose your dist folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    let ctx = {};
+app.use((req, res, next) => {
     if (req.headers['hx-request']) {
-        ctx.layout = null;
+        req.ctx = { layout : null };
     }
-    res.render("pages/index", ctx);
+    next();
 });
 
-app.get("/about", (req, res) => {
-    let ctx = {};
-    if (req.headers['hx-request']) {
-        ctx.layout = null;
-    }
-    res.render("pages/about", ctx);
-});
-
-app.get("/contact", (req, res) => {
-    let ctx = {};
-    if (req.headers['hx-request']) {
-        ctx.layout = null;
-    }
-    res.render("pages/contact", ctx);
-});
-
-app.get("/teams", (req, res) => {
-    let ctx = {};
-    if (req.headers['hx-request']) {
-        ctx.layout = null;
-    }
-    res.render("pages/teams", ctx);
-});
-
-app.get("/blog", (req, res) => {
-    let ctx = {};
-    if (req.headers['hx-request']) {
-        ctx.layout = null;
-    }
-    res.render("pages/blog", ctx);
-});
-
-app.get("/login", (req, res) => {
-    let ctx = {};
-    if (req.headers['hx-request']) {
-        ctx.layout = null;
-    }
-    res.render("pages/login", ctx);
-});
+app.use('/', homeRoute);
+app.use('/about', aboutRoute);
+app.use('/contact', contactRoute);
+app.use('/blog', blogRoute);
+app.use('/teams', teamsRoute);
+app.use('/login', loginRoute);
 
 app.listen(port, () => {
     console.log("Server running");
