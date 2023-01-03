418f7596780a9d16348ad9119f593c805f620c4a
believelody <believelody@gmail.com>
2023-01-03 03:39:13 +0100

    improved blog route error handling

--- routes/blog/index.js
+++ routes/blog/index.js
@@ -3,14 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let blogRes = {};
     const limitArray = ['6', '18', '30'];
     const limit = limitArray.includes(req.query.limit) ? req.query.limit : limitArray[0]; 
     const page = parseInt(req.query.page) || 1;
-    const totalPage = parseInt(req.query.pages) || 1;
-    if (page < 1 || page > totalPage) {
-        blogRes.error = "Aucun contenu n'a été trouvé."
-    } else {
         const postsRes = await fetch(`${process.env.DUMMY_DATA_URL}/posts?limit=${limit}&skip=${parseInt(limit) * (page - 1)}`);
         const postsJson = await postsRes.json();
         const { posts, total } = postsJson;
@@ -26,9 +21,7 @@ router.get('/', async (req, res) => {
             id: post.id,
             userId: post.userId,
         }));
-        blogRes = { blogs, meta: { pages: Math.round(total / parseInt(limit)), page, limit } }
-    }
-    req.ctx = { ...req.ctx, ...blogRes, title: 'Blogs' };
+    req.ctx = { ...req.ctx, blogs, meta: { pages: Math.round(total / parseInt(limit)), page, limit }, title: 'Blogs' };
     return res.render('pages/blog', req.ctx)
 });
 
