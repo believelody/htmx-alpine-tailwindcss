28286db8a0e53a668b28a29d25e02ec90a5676a0
believelody <believelody@gmail.com>
2023-01-07 15:52:03 +0100

    added scroll to top via hx-swap in blog list

--- routes/login/index.js
+++ routes/login/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    console.log("user session : ", req.session.user);
     if (req.session.user) {
         return res.redirect("/");
     }
     req.ctx = { ...req.ctx, title: 'Login Page' }
-    return res.render('pages/login', req.ctx)
+    return res.render('pages/login', req.ctx);
 });
 
 export default router;
