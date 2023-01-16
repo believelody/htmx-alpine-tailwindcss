aaa6c64a2a70765c188afaf5b60d08f5e8e82637
believelody <believelody@gmail.com>
2023-01-17 00:32:47 +0100

    improved notification feature

--- routes/api/contact/index.js
+++ routes/api/contact/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
+router.post("/1", async (req, res, next) => {
   try {
     console.log(req.body);
     await new Promise(resolve => setTimeout(resolve, 3000));
@@ -10,6 +10,18 @@ router.post("/", async (req, res, next) => {
     return res.render('partials/form/contact', {
       ...req.ctx,
       notify: { type: 'success', title: 'Contact', message: 'Nous avons bien reçu votre demande. Nous reviendrons vers vous dans les plus brefs délai.', }
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+router.post("/2", async (req, res, next) => {
+  try {
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    return res.json({
+      notify: { type: 'success', title: 'Contact', message: 'Nous avons bien reçu votre demande. Nous reviendrons vers vous dans les plus brefs délai.', timeout: 10000 }
     });
   } catch (error) {
     console.log(error);
