18d12da064180cd7a30f6e9b916d972933bbd926
believelody <believelody@gmail.com>
2023-01-20 01:08:47 +0100

    add position and slideDirection attributes to notification

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
       animation: {
         'slide-in-left': 'slide-in-left 1s ease-in-out forwards',
         'slide-out-left': 'slide-out-left 1s ease-in-out forwards',
+        'slide-in-right': 'slide-in-right 1s ease-in-out forwards',
+        'slide-out-right': 'slide-out-right 1s ease-in-out forwards',
       },
       colors: ({ colors }) => ({
         ...colors,
@@ -26,9 +28,6 @@ module.exports = {
             transform: 'translateX(-100%)',
             'max-height': 0,
           },
-          '25%': {
-            opacity: 1,
-          },
           '100%': {
             opacity: 1,
             transform: 'translateX(0)',
@@ -41,12 +40,33 @@ module.exports = {
             transform: 'translateX(0)',
             'max-height': '100vh',
           },
-          '75%': {
-            opacity: 1,
-          },
           '100%': {
             opacity: 0,
             transform: 'translateX(-100%)',
+            'max-height': 0,
+          },
+        },
+        'slide-in-right': {
+          '0%': {
+            opacity: 0,
+            transform: 'translateX(100%)',
+            'max-height': 0,
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(0)',
+            'max-height': '100vh',
+          }
+        },
+        'slide-out-right': {
+          '0%': {
+            opacity: 1,
+            transform: 'translateX(0)',
+            'max-height': '100vh',
+          },
+          '100%': {
+            opacity: 0,
+            transform: 'translateX(100%)',
             'max-height': 0,
           },
         }
