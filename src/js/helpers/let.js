9a823e424026894ccaf2bd726db64df0c906991a
believelody <believelody@gmail.com>
2023-02-02 15:29:46 +0100

    improved let helper

--- src/js/helpers/let.js
+++ src/js/helpers/let.js
@@ -1,9 +1,5 @@
-export default function (options) {
-    const data = {};
-    let out = "";
-    Object.entries(options.hash).forEach(([key, value]) => {
-        data[key] = value;
-        out += options.fn(data);
-    });
-    return out;
+export default function ({ hash, fn }) {
+    hash = { ...hash, ...hash.spread };
+    delete hash.spread;
+    return fn(hash);
 }
