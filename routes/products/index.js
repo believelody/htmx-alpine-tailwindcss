201aa91079a335b3cb2d6d24f5ad470b3d2bea2e
believelody <believelody@gmail.com>
2023-02-06 09:12:42 +0100

    fixed product page refresh with count query

--- routes/products/index.js
+++ routes/products/index.js
@@ -11,14 +11,14 @@ router.get('/', limitQueryValidator, async (req, res, next) => {
   try {
     const limit = Number(req.query.limit || limitArray[0]);
     const count = Number(req.query.count || limit);
-    if (req.query.count) {
+    if (req.query.count && req.ctx.fromHTMX) {
       const { products, total } = await service.product.fetchAll(limit, count - limit);
       req.ctx = { ...req.ctx, meta: { total, limit, count } };
       req.session.meta = { total, limit, count };
       res.setHeader('HX-Trigger', 'update-context');
       return res.render('partials/product/list', { ...req.ctx, products });
     }
-    const { products, total } = await service.product.fetchAll(limit, 0);
+    const { products, total } = await service.product.fetchAll(count, 0);
     return res.render('pages/product', { ...req.ctx, products, meta: { total, limit, count }, title: productsTitle });
   } catch (error) {
     console.log(`In ${req.originalUrl} route`);
